3d9514455fc98133265e51d1767b5a7aa3fb51e1
olivia kaufmann <olivia.kaufmann@ilub.unibe.ch>
2021-04-07 15:42:58 +0200

    Update for ilias7

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id                = 'grafana';
 $version           = '1.0.0';
 $ilias_min_version = '5.3.0';
-$ilias_max_version = '6.999.999';
+$ilias_max_version = '7.999.999';
 $responsible       = 'iLUB';
 $responsible_mail  = 'olivia.kaufmann@ilub.unibe.ch';
